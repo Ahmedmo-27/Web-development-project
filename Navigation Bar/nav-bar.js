2229303d381f3e30ef949977c79f75a3c572b718
Ahmedmo-27 <ahmed2303479@miuegyp.edu.eg>
2025-03-14 08:41:27 +0200

    Updated Code With Modifications

--- Navigation Bar/nav-bar.js
+++ Navigation Bar/nav-bar.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     window.addEventListener('resize', () => {
         if (window.innerWidth < 1100) {
+            logo.style.position = 'static';
             searchButton.style.display = 'none';
             searchButton2.style.marginLeft = '50px';
             middleDiv.style.display = 'flex';
@@ -27,81 +28,14 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    
-
-    //nav bar scroll
-    /*window.addEventListener('scroll', function () {
-        const headerTop = document.getElementById('header-top');
-        const sideicon = document.getElementById('side-icon');
-        const logo = document.getElementById('logo');
-
-        if (window.scrollY > 0) {
-            header.classList.remove('header-unscrolled');
-            header.classList.add('header-scrolled');
-
-            searchButton.style.display = 'none';
-
-            searchButton2.classList.remove('search-button-unscrolled');
-            searchButton2.classList.add('search-button-scrolled');
-
-            sideicon.style.color = 'black';
-
-            middleDiv.style.display = 'flex';
-
-            headerTop.style.display = 'none';
-
-            middleDiv.classList.remove('header-top-unscrolled');
-            middleDiv.classList.add('header-top-scrolled');
-
-            headerMiddle.style.justifyContent = 'space-between';
-
-            logo.classList.remove('logo-unscrolled');
-            logo.classList.add('logo-scrolled');
-
-            headerBottom.classList.remove('header-bottom-unscrolled');
-            headerBottom.classList.add('header-bottom-scrolled');
-        } else {
-            header.classList.remove('header-scrolled');
-            header.classList.add('header-unscrolled');
-
-            if (window.innerWidth > 1100) {
-                searchButton.style.display = 'flex';
-                middleDiv.style.display = 'none';
-            } else {
-                middleDivanchors.forEach(anchor => {
-                    anchor.style.display = 'none';
-                });
-                headerMiddle.style.justifyContent = 'center';
-            }
-
-            searchButton2.classList.remove('search-button-scrolled');
-            searchButton2.classList.add('search-button-unscrolled');
-
-            sideicon.style.color = 'white';
-
-            headerTop.style.display = 'flex';
-
-            middleDiv.classList.remove('header-top-unscrolled');
-            middleDiv.classList.remove('header-top-scrolled');
-
-            headerMiddle.style.justifyContent = 'center';
-
-            logo.classList.remove('logo-scrolled');
-            logo.classList.add('logo-unscrolled');
-
-            headerBottom.classList.add('header-bottom-unscrolled');
-            headerBottom.classList.remove('header-bottom-scrolled');
-        }
-    });*/
-
-
-
-    window.addEventListener('scroll', function () {
+    window.addEventListener('scroll', function () 
+    {
         const headerTop = document.getElementById('header-top');
         const sideicon = document.getElementById('side-icon');
         const logo = document.getElementById('logo');
     
-        if (window.scrollY > 0) {
+        if (window.scrollY > 0) 
+            {
             header.classList.remove('header-unscrolled');
             header.classList.add('header-scrolled');
     
@@ -133,7 +67,10 @@ document.addEventListener('DOMContentLoaded', function () {
             logo.classList.add('logo-scrolled');
             headerBottom.classList.remove('header-bottom-unscrolled');
             headerBottom.classList.add('header-bottom-scrolled');
-        } else {
+        } 
+        
+        else 
+        {
             header.classList.remove('header-scrolled');
             header.classList.add('header-unscrolled');
     
@@ -196,86 +133,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const headerExtension = document.getElementById('header-bottom-anchor-extension');
     const exitButton = document.getElementById('exit-extension-button');
     let mouseOverLink = false; //in case el user alternated between el links besor3a msh 3aizeen flickering
-
-    /*const brandsLink = document.querySelector('a[data-category="Brands"]');
-    const brandsExtension = document.getElementById('header-bottom-brands-extension');
-    const exitBrandsButton = document.getElementById('exit-brands-extension-button');
-    let mouseOverBrandsLink = false;
-    let mouseOverBrandsExtension = false;
-
-if (brandsLink && brandsExtension) {
-    brandsLink.addEventListener('mouseenter', () => {
-        mouseOverBrandsLink = true;
-        brandsExtension.style.display = 'block';
-    });
-
-    brandsLink.addEventListener('mouseleave', () => {
-        mouseOverBrandsLink = false;
-        setTimeout(() => {
-            if (!mouseOverBrandsExtension) {
-                brandsExtension.style.display = 'none';
-            }
-        }, 30);
-    });
-
-    brandsExtension.addEventListener('mouseenter', () => {
-        mouseOverBrandsExtension = true;
-    });
-
-    brandsExtension.addEventListener('mouseleave', () => {
-        mouseOverBrandsExtension = false;
-        setTimeout(() => {
-            if (!mouseOverBrandsLink) {
-                brandsExtension.style.display = 'none';
-            }
-        }, 30);
-    });
- }
-
-    if (exitBrandsButton) {
-        exitBrandsButton.addEventListener('click', () => {
-            brandsExtension.style.display = 'none';
-        });
-    }
-
-
-if (exitButton) {
-    exitButton.addEventListener('click', () => {
-        mouseOverLink = false;
-        console.log('exit button clicked');
-        headerExtension.style.display = 'none';
-    });
-   }
-
-    navigationLinks.forEach(link => {
-    link.addEventListener('mouseenter', () => {
-        mouseOverLink = true;
-        headerExtension.style.display = 'block';
-    });
-
-    link.addEventListener('mouseleave', () => {
-        mouseOverLink = false;
-        setTimeout(() => {
-            if (!mouseOverLink) {
-                headerExtension.style.display = 'none';
-            }
-        }, 30);
-    });
-     });
-
-    headerExtension.addEventListener('mouseenter', () => {
-    mouseOverLink = true;
-    });
-
-     headerExtension.addEventListener('mouseleave', () => {
-    mouseOverLink = false;
-    setTimeout(() => {
-        if (!mouseOverLink) {
-            headerExtension.style.display = 'none';
-        }
-    }, 30);
-   });*/
-
 
     const braceletsLink = document.querySelector('a[data-category="Bracelets"]');
     const braceletsExtension = document.getElementById('header-bottom-bracelets-extension');
@@ -391,37 +248,6 @@ if (exitButton) {
         }
     }
 
-    /*searchField.addEventListener('input', async () => {
-        const query = searchField.value.trim();
-        if (query.length > 2) {
-            headerBottom.style.display = 'none';
-            header.classList.remove('header-unscrolled');
-            header.classList.add('header-scrolled');
-            searchExtension.style.display = 'flex';
-            if (query.length > 0) {
-                const response = await fetch('/user/search', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ query })
-                });
-
-                const { products } = await response.json();
-                displaySearchResults(products);
-            } else {
-                searchResultsDiv.innerHTML = ''; // Clear results if input is empty
-                searchExtension.style.display = 'none'; // Hide the extension
-            }
-        } else {
-            searchExtension.style.display = 'none';
-            header.classList.remove('header-scrolled');
-            header.classList.add('header-unscrolled');
-        }
-    });*/
-
-
-
     searchField.addEventListener('input', async () => {
         const query = searchField.value.trim();
         if (query.length > 2) {
@@ -486,13 +312,6 @@ if (exitButton) {
         });
         searchExtension.style.display = 'flex';
     }
-
-    /*document.getElementById('exit-search-extension-button').addEventListener('click', () => {
-        searchExtension.style.display = 'none';
-        searchField.value = '';
-        header.classList.remove('header-scrolled');
-        header.classList.add('header-unscrolled');
-    });*/
 
     document.getElementById('exit-search-extension-button').addEventListener('click', () => {
         searchExtension.style.display = 'none';
